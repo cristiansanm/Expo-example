8c156d1c7b646e80f2a385e60d10a66db2236d4e
crissanzm <krissanz13@gmail.com>
2024-04-26 23:49:56 +0200

    fixing issues with imports

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,13 +2,15 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@components/EditScreenInfo';
 import { Text, View } from '@components/Themed';
+import { usePathname, useRouter } from 'expo-router';
 
 export default function TabOneScreen() {
+  const pathName = usePathname()
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path={pathName} />
     </View>
   );
 }
